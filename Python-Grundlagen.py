6cea41ef9775494796f02a3f63e539080205fe10
simxn <171wita27@o365.htl-leoben.at>
2018-04-23 14:21:08 +0200

    python file expanded

---
+++ Python-Grundlagen.py
@@ -0,0 +1,18 @@
+# Python-Grundlagen.py
+
+# Kommentare erfolgen mit hashtag
+
+# Ausgabe von Daten
+print("Hallo World")
+
+# Variable definieren (kann ohne Typ erfolgen)
+heimat = "Erde"
+
+# Mehrere Variablen werden mit "," getrennt
+print(heimat, "an World:, Hallo!")
+
+# Eingabe / liest Text(!) von der Konsole ein
+wer = input("Und wer bist du?")
+
+# und gibt den Text wieder aus
+print("Hallo", wer)
