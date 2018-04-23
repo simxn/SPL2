7926f1fc983e00c1177db8c5572c0a71e48d71e7
simxn <171wita27@o365.htl-leoben.at>
2018-04-23 16:43:03 +0200

    tried to improve Zahlenraten.py

--- Zahlenraten.py
+++ Zahlenraten.py
@@ -11,16 +11,18 @@ Czahl = maxzahl/2
 Czahl = int(Czahl)
 versuche = 0
 
+rechnung = Czahl
+
 while(erraten == False):
     rechnung = int(Czahl/2)
     versuche += 1
     print(rechnung)
     if(rechnung < zahl):
-        Czahl = Czahl + Czahl/4 * 3
-    if(rechnung < zahl):
-        Czahl = Czahl - Czahl/4 * 3
-    if(rechnung == zahl):
-        erraten == True 
+        Czahl = int(Czahl + Czahl/4 * 3)
+    elif(rechnung > zahl):
+        Czahl = int(Czahl - Czahl/4 * 3)
+    elif(rechnung == zahl):
+        erraten = True 
     if(versuche == 10): 
         break
 print("Der Computer benötigte", versuche, "Versuche um die Zahl", zahl, "zu erraten.")
